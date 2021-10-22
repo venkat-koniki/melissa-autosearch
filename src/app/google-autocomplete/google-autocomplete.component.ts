026059e4c1c1db40fdc46d23d42cb5234610887a
venkat-koniki <67361000+venkat-koniki@users.noreply.github.com>
2021-10-22 22:22:56 +0530

    auto

--- src/app/google-autocomplete/google-autocomplete.component.ts
+++ src/app/google-autocomplete/google-autocomplete.component.ts
@@ -13,17 +13,43 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class GoogleAutocompleteComponent implements OnInit {
 
-  addressFormGroup: FormGroup|any;
+  // addressFormGroup: FormGroup|any;
   addressFormGroup2: FormGroup|any;
   ngOnInit(): void {
-    this.addressFormGroup = new FormGroup({
-      address: new FormControl(),
-    });
+    // this.addressFormGroup = new FormGroup({
+    //   address: new FormControl(),
+    // });
     this.addressFormGroup2 = new FormGroup({
       address: new FormControl(),
+      address2:new FormControl(),
+      address1:new FormControl(),
+      city:new FormControl(),
+      zipcode:new FormControl(),
+      country:new FormControl()
     });
-    this.addressFormGroup.get('address').valueChanges.subscribe((value:any) => console.log('value changed', value));
-    this.addressFormGroup2.get('address').valueChanges.subscribe((value:any) => console.log('value changed', value))
+    // this.addressFormGroup.get('address').valueChanges.subscribe((value:any) => console.log('value changed', value));
+    this.addressFormGroup2.get('address').valueChanges.subscribe((value:any) =>{
+       this.updateFormValue(value); 
+    })
+  }
+
+  updateFormValue(value:any){
+    console.log(value.address_components);
+    let acountry=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("country"));
+    let aZip=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("postal_code"));
+    let aCity=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("locality"));
+
+    let stnumber=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("street_number"));
+    let route=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("route"));
+    let neighborhood=value.address_components.find((ad: { types: string | any[]; })=>ad.types.includes("neighborhood"));
+    //console.log(aCity);
+    this.addressFormGroup2.patchValue({
+      address2:stnumber?.long_name+route?.long_name,
+      address1:neighborhood?.long_name,
+      city:aCity?.long_name,
+      country: acountry?.long_name, 
+      zipcode:aZip?.long_name   
+    });
   }
 
 }
